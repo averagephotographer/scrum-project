02b4276ba9e337d99ff05e4312738525e8c50a20
averagephotographer <christopher.tullier98@gmail.com>
2021-10-30 12:08:11 -0500

    created item class

--- Project/MyGameLibrary/Character.cs
+++ Project/MyGameLibrary/Character.cs
@@ -4,66 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Fall2020_CSC403_Project.code {
-  public class Character {
-    private const int GO_INC = 3;
-
-    public Vector2 MoveSpeed { get; private set; }
-    public Vector2 LastPosition { get; private set; }
-    public Vector2 Position { get; private set; }
-    public Collider Collider { get; private set; }
-    private List<string> Inventory = new List<string>();
-
-
-        public Character(Vector2 initPos, Collider collider) {
-      Position = initPos;
-      Collider = collider;
-    }
-
-    public void Move() {
-      LastPosition = Position;
-      Position = new Vector2(Position.x + MoveSpeed.x, Position.y + MoveSpeed.y);
-      Collider.MovePosition((int)Position.x, (int)Position.y);
-    }
-
-    public void MoveBack() {
-      Position = LastPosition;
-    }
-
-    public void GoLeft() {
-      MoveSpeed = new Vector2(-GO_INC, 0);
-    }
-    public void GoRight() {
-      MoveSpeed = new Vector2(+GO_INC, 0);
-    }
-    public void GoUp() {
-      MoveSpeed = new Vector2(0, -GO_INC);
-    }
-    public void GoDown() {
-      MoveSpeed = new Vector2(0, +GO_INC);
-    }
-
-    public void ResetMoveSpeed() {
-      MoveSpeed = new Vector2(0, 0);
-    }
-    public void InventoryAdd(string item)
+namespace Fall2020_CSC403_Project.code
+{
+    public class Character
     {
-        Console.WriteLine("Adding: ");
-        Inventory.Add(item);
-        Inventory.ForEach(Console.Write);
-        Console.WriteLine();
-        Console.WriteLine();
+        private const int GO_INC = 3;
+
+        public Vector2 MoveSpeed { get; private set; }
+        public Vector2 LastPosition { get; private set; }
+        public Vector2 Position { get; private set; }
+        public Collider Collider { get; private set; }
+        private List<string> Inventory = new List<string>();
+
+
+        public Character(Vector2 initPos, Collider collider)
+        {
+            Position = initPos;
+            Collider = collider;
+        }
+
+        public void Move()
+        {
+            LastPosition = Position;
+            Position = new Vector2(Position.x + MoveSpeed.x, Position.y + MoveSpeed.y);
+            Collider.MovePosition((int)Position.x, (int)Position.y);
+        }
+
+        public void MoveBack()
+        {
+            Position = LastPosition;
+        }
+
+        public void GoLeft()
+        {
+            MoveSpeed = new Vector2(-GO_INC, 0);
+        }
+        public void GoRight()
+        {
+            MoveSpeed = new Vector2(+GO_INC, 0);
+        }
+        public void GoUp()
+        {
+            MoveSpeed = new Vector2(0, -GO_INC);
+        }
+        public void GoDown()
+        {
+            MoveSpeed = new Vector2(0, +GO_INC);
+        }
+
+        public void ResetMoveSpeed()
+        {
+            MoveSpeed = new Vector2(0, 0);
+        }
+        public void InventoryAdd(string item)
+        {
+            Console.WriteLine("Adding: ");
+            Inventory.Add(item);
+            Inventory.ForEach(Console.Write);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        // eventually made items a class?
+        public string InventoryRemove(int index)
+        {
+            Console.WriteLine("Removing: ");
+            string item = Inventory[index];
+            Inventory.RemoveAt(index);
+            Inventory.ForEach(Console.Write);
+            Console.WriteLine();
+            Console.WriteLine();
+            return item;
+        }
     }
-    // eventually made items a class?
-    public string InventoryRemove(int index)
-    {
-        Console.WriteLine("Removing: ");
-        string item = Inventory[index];
-        Inventory.RemoveAt(index);
-        Inventory.ForEach(Console.Write);
-        Console.WriteLine();
-        Console.WriteLine();
-        return item;
-    }
-  }
 }
---
+++ Project/MyGameLibrary/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public class Item
+    {
+        public Vector2 Position { get; private set; }
+        public Collider Collider { get; private set; }
+        public string Name { get; private set; }
+
+        public Item(string name, Vector2 initPos, Collider collider)
+        {
+            Name = name;
+            Position = initPos;
+            Collider = collider;
+        }
+    }
+}
